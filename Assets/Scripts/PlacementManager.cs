1079313ce3c5ebacf1ab422cf2023f180c442cf0
Samyhrn <samy.harani@outlook.fr>
2023-06-29 20:06:32 +0200

    added snapping system for towers
    Les tours défensives se snappent aux tuiles de la map, et on ne peut pas placer de tour sur une autre, ni sur le chemin des énemis

---
+++ Assets/Scripts/PlacementManager.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementManager : MonoBehaviour
+{
+    public GameObject basicTowerObject;
+
+    private GameObject dummyPlacement;
+
+    private GameObject hoverTile;
+
+    public Camera cam;
+
+    public LayerMask mask;
+    public LayerMask towerMask;
+
+    public bool isBuilding;
+
+    public void Start()
+    {
+        StartBuilding();
+    }
+
+    public Vector2 GetMousePosition()
+    {
+        return cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    public void GetCurrentHoverTile()
+    {
+        Vector2 mousePosition = GetMousePosition();
+
+
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, new Vector2(0,0), 0.1f, mask, -100, 100);
+        if (hit.collider != null)
+        {
+            if (MapGenerator.mapTiles.Contains(hit.collider.gameObject))
+            {
+                if (!MapGenerator.pathTiles.Contains(hit.collider.gameObject))
+                {
+                    hoverTile = hit.collider.gameObject;
+                }
+            }
+        }
+    }
+
+    public bool CheckForTower()
+    {
+        bool TowerOnSlot = false;
+        Vector2 mousePosition = GetMousePosition();
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, new Vector2(0,0), 0.1f, towerMask, -100, 100);
+
+        if (hit.collider != null)
+        {
+            TowerOnSlot = true;
+        }
+      
+
+        return TowerOnSlot;
+    }
+
+
+    public void PlaceBuilding()
+    {
+        if (hoverTile!=null)
+        {
+            if (CheckForTower() == false)
+            {
+                GameObject newTowerObject= Instantiate(basicTowerObject);
+                newTowerObject.layer= LayerMask.NameToLayer("Tower");
+                newTowerObject.transform.position = hoverTile.transform.position;
+
+                EndBuilding();
+
+            }
+            
+        }
+    }
+
+
+    public void StartBuilding()
+    {
+        isBuilding = true;
+        
+        dummyPlacement = Instantiate(basicTowerObject);
+
+        if (dummyPlacement.GetComponent<Tower>() != null)
+        {
+            Destroy(dummyPlacement.GetComponent<Tower>());
+        }
+
+        if (dummyPlacement.GetComponent<BarrelRotation>() != null)
+        {
+            Destroy(dummyPlacement.GetComponent<BarrelRotation>());
+        }
+    }
+
+    public void EndBuilding()
+    {
+        isBuilding = false;
+
+        if (dummyPlacement != null)
+        {
+            Destroy(dummyPlacement);
+        }
+    }
+
+    public void Update()
+    {
+        if (isBuilding==true)
+        {
+            if (dummyPlacement != null)
+            {
+                GetCurrentHoverTile();
+                if (hoverTile != null)
+                {
+                    dummyPlacement.transform.position = hoverTile.transform.position;
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isBuilding == true)
+            {
+                PlaceBuilding();
+            }
+        }
+    }
+}
