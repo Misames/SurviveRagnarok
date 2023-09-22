29e6d0fbfe49fba9aac6bb543ad5b036ddddd17d
François Bouscaillou <francois.bouscaillou@gmail.com>
2023-09-22 14:53:02 +0200

    feature (BT): add basics class for BT Node Selector Sequence

---
+++ Assets/Scripts/BehaviourTree/Node.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+
+namespace BehaviorTree
+{
+    public enum NodeState
+    {
+        RUNNING,
+        SUCCESS,
+        FAILURE
+    }
+
+    public class Node
+    {
+        protected NodeState state;
+        protected Node parent;
+        protected List<Node> children = new();
+
+        public Node()
+        {
+            parent = null;
+        }
+
+        public Node(List<Node> children)
+        {
+            foreach (Node child in children)
+                Attach(child);
+        }
+
+        private void Attach(Node node)
+        {
+            node.parent = this;
+            children.Add(node);
+        }
+
+        public virtual NodeState Evaluate()
+        {
+            foreach (Node child in children)
+            {
+                switch (child.Evaluate())
+                {
+                    case NodeState.FAILURE:
+                        state = NodeState.FAILURE;
+                        return state;
+                    case NodeState.SUCCESS:
+                        state = NodeState.SUCCESS;
+                        continue;
+                    case NodeState.RUNNING:
+                        state = NodeState.RUNNING;
+                        break;
+                    default:
+                        state = NodeState.SUCCESS;
+                        return state;
+                }
+            }
+            return state;
+        }
+    }
+}
